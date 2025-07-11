3c1cfe6811a1052fe0bdeb8e942e77742bbbb481
Dev-PKH <tapasse12@gmail.com>
2025-07-11 18:58:36 +0900

    feat: 일부 추가 셋팅
    - 씨작신부터 실행 추가(EditPlayScene)
    - 멀티씬으로 씬 분류 및 로드 연동

---
+++ Assets/Scripts/Manager/LoadSceneManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public enum SceneName
+{
+    Core,
+    Main,
+    None
+}
+
+public class LoadSceneManager : MonoBehaviour
+{
+    private static LoadSceneManager instance;
+    public static LoadSceneManager Instance => instance;
+
+    private void Awake()
+    {
+        if (Instance == null) instance = this;
+        LoadScene(SceneName.Main);
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadScene(SceneName sn)
+    {
+        SceneManager.LoadSceneAsync((int)sn, LoadSceneMode.Additive);
+    }
+
+    public void ChangeScene(SceneName nextScene, SceneName curScene)
+    {
+        StartCoroutine(LoadScene(nextScene, curScene));
+    }
+
+    public IEnumerator LoadScene(SceneName nextScene, SceneName curScene)
+    {
+        yield return null;
+        AsyncOperation scheduler = SceneManager.LoadSceneAsync((int)nextScene, LoadSceneMode.Additive);
+        scheduler.allowSceneActivation = false;
+        while(!scheduler.isDone)
+        {
+            yield return null;
+            if(scheduler.progress >= 0.9f)
+            {
+                scheduler.allowSceneActivation = true;
+                yield return null;
+            }
+        }
+        SceneManager.UnloadSceneAsync((int)curScene);
+    }
+}
