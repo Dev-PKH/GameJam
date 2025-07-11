021480d040909f7857a032367f1ce8582f05d6ff
linak31413 <linak31413@gmail.com>
2025-07-12 02:13:32 +0900

    feat: New event data + No more repeated event
    Added event 01, 02, 03, 04, 05, 06, 07, 08, 11, 12, 13.
    Prevented events to not be repeated.

--- Assets/Scripts/Event/EventController.cs
+++ Assets/Scripts/Event/EventController.cs
@@ -5,6 +5,7 @@ public class EventController : MonoBehaviour
 	[SerializeField] private EventManager eventManager;
 	[SerializeField] private EventData[] allEvents; // 여러 ScriptableObject들
 
+	int prevIndex = -1;
 	bool isActive = false;
 
 	private void Update()
@@ -22,32 +23,51 @@ public class EventController : MonoBehaviour
         }
 	}
 
-	public void TriggerRandomEvent()
+    // 이번에 올 이벤트의 인덱스를 구함
+	public int FetchEventIndex()
 	{
-		if (allEvents.Length == 0)
-		{
-			Debug.LogWarning("이벤트 목록이 비어 있음!");
-			return;
-		}
-
+        // 선택 가중치를 전부 더하여 선택 풀을 만든다
 		int eventPickMax = allEvents.Length;
 		for (int i = 0; i < allEvents.Length; i++)
 		{
             eventPickMax += allEvents[i].chanceMult;
         }
+        // 선택 풀에서 랜덤 정수를 선택
         int eventPick = Random.Range(0, eventPickMax);
-		int index = 0;
+		int returnIndex = 0;
         for (int i = 0; i < allEvents.Length; i++)
         {
+            // 각 이벤트별 가중치를 하나씩 빼기
             eventPick -= allEvents[i].chanceMult;
-			if (eventPick < 0)
+			if (eventPick < 0) // 랜덤 정수가 음수가 되면 당선
 			{
-				index = i;
+                returnIndex = i;
                 break;
 			}
         }
-        EventData selected = allEvents[index];
 
-		eventManager.ShowEvent(selected);
-	}
+        // 이전 이벤트와 중복됨 -> 다시
+        if (returnIndex == prevIndex) {
+            return FetchEventIndex();
+        }
+        // 이전 이벤트가 중복되지 않았음 -> 전달
+        return returnIndex;
+    }
+
+    public void TriggerRandomEvent()
+    {
+        if (allEvents.Length == 0)
+        {
+            Debug.LogWarning("이벤트 목록이 비어 있음!");
+            return;
+        }
+
+        // 이전 이벤트와 겹치지 않는 이벤틑 인덱스를 가져옴
+        int index = FetchEventIndex();
+        EventData selected = allEvents[index];
+        // 이전 이벤트의 인덱스를 갱신
+        prevIndex = index;
+
+        eventManager.ShowEvent(selected);
+    }
 }
