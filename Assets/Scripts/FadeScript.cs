02c430e39aa3ed94a218f3798d39064a2c0638ac
linak31413 <linak31413@gmail.com>
2025-07-12 09:07:50 +0900

    feat: 페이드 인/아웃 기능
    Scene에 페이드용 검은 이미지 추가
    페이드 인/아웃 기능 함수 추가

---
+++ Assets/Scripts/FadeScript.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeScript : MonoBehaviour
+{
+    public Image Panel;
+    float time = 0f;
+    float F_time = 1f;
+
+    public void FadeIn()
+    {
+        StartCoroutine(FadeInFlow());
+    }
+    public void FadeOut()
+    {
+        StartCoroutine(FadeOutFlow());
+    }
+
+    IEnumerator FadeInFlow()
+    {
+        time = 0f;
+        Panel.gameObject.SetActive(true);
+        Color alpha = Panel.color;
+        // 페이드 인 진행 중
+        while (alpha.a < 1f)
+        {
+            time += Time.deltaTime / F_time;
+            alpha.a = Mathf.Lerp(0, 1, time);
+            Panel.color = alpha;
+            yield return null;
+        }
+        yield return null; 
+    }
+
+    IEnumerator FadeOutFlow()
+    {
+        time = 0f;
+        Color alpha = Panel.color;
+        // 페이드 아웃 진행 중
+        while (alpha.a > 0f)
+        {
+            time += Time.deltaTime / F_time;
+            alpha.a = Mathf.Lerp(1, 0, time);
+            Panel.color = alpha;
+            yield return null;
+        }
+        // 페이드 아웃 완료
+        Panel.gameObject.SetActive(false);
+        yield return null;
+    }
+}
