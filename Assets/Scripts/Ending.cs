00276ed80ff005c26bb096cf5cbf3863cf33c395
Dev-PKH <tapasse12@gmail.com>
2025-07-13 07:03:30 +0900

    fix: 이미지 및 로직수정
    - 엔딩 설정

---
+++ Assets/Scripts/Ending.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ending : MonoBehaviour
+{
+    public PauseMenu pauseMenu;
+
+    private void Awake()
+    {
+        pauseMenu = FindObjectOfType<PauseMenu>();
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void MainMenuEnter()
+    {
+        pauseMenu.MainMenuButton();
+    }
+}
